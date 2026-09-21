6a45f192449fe15e69140cfcae75fda42c244764
OrlyBarr <orlywaitzer12@gmail.com>
2026-09-21 23:30:08 +0300

    Data quality test

--- dags/main.py
+++ dags/main.py
@@ -1,4 +1,5 @@
-from airflow import DAG 
+from airflow import DAG
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 import pendulum
 from datetime import datetime, timedelta
 from api.video_stats import get_playlist_id, get_video_ids, extract_video_data, save_to_json
@@ -35,16 +36,20 @@ with DAG(
     video_ids = get_video_ids(playlist_id)
     extract_data = extract_video_data(video_ids)
     save_to_json_task = save_to_json(extract_data)
+    trigger_update_db = TriggerDagRunOperator(
+        task_id='trigger_update_db',
+        trigger_dag_id='update_db',
+    )
 
     # Define dependencies
-    playlist_id >> video_ids >> extract_data >> save_to_json_task
+    playlist_id >> video_ids >> extract_data >> save_to_json_task >> trigger_update_db
 
 
 with DAG(
     dag_id= 'update_db',
     default_args=default_args,
     description= 'DAG to process JSON file and insert data into both staging and core schemas',
-    schedule='0 15 * * *',
+    schedule=None,
     catchup=False
 ) as dag:
 
